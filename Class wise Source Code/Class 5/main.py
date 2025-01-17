12bb9114247688b0db84aaef1e24624ba0835c22
tajinstituteofit <taj.instit@gmail.com>
2025-01-17 13:01:33 +0500

    Add files via upload

---
+++ Class wise Source Code/Class 5/main.py
@@ -0,0 +1,18 @@
+first_name = "ali"
+last_name = "hassan"
+
+"first_name last_name"
+
+# abc = f"Hello Mr.{first_name} {last_name}"
+
+# cde = first_name, last_name
+
+# print(abc)
+
+
+
+abc = f"{first_name} {last_name}"
+
+cde = f"My student name is: {abc.upper()}"
+print(first_name)
+print(last_name)
