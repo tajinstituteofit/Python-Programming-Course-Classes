2860eeb14b790c9fe69560c84c7280a243e54fc1
tajinstituteofit <taj.instit@gmail.com>
2025-01-23 15:11:27 +0500

    Add files via upload

---
+++ Class wise Source Code/Class 8/main.py
@@ -0,0 +1,37 @@
+# Numbers
+
+# abc = 50
+# cde = 100
+
+# result = abc + cde
+
+# print(result)
+
+
+
+# abc = 50
+# cde = "100"
+
+# result = abc + cde
+
+# print(result)
+
+
+
+abc = 50
+cde = "60"
+value = 150
+
+print(f"{abc}\n {cde}\n {value}\n")
+
+
+
+# print(type(abc) )
+# print(type(cde) )
+
+
+# print(abc - value)
+# print(abc * value)
+# print(abc / value)
+
+# print(4**2)
