1f103f8c49a9a5ced9c9a8106cfe47cba5408bf5
tajinstituteofit <taj.instit@gmail.com>
2025-02-01 20:00:05 +0500

    Add files via upload

---
+++ Class wise Source Code/Class 10/main.py
@@ -0,0 +1,13 @@
+abc  = [10, 11, 12, 13]
+
+print(abc)
+
+
+def_ =          ['ali', 'usama', 'yasir', 'hanzla']
+# index_number     0        1        2        3
+
+
+
+print(def_[2])
+print(def_[1])
+print(def_[0])
---
+++ Class wise Source Code/Class 11/main.py
@@ -0,0 +1,16 @@
+students = ["usama", 'ali', 'yasir', 'ABDULLAH']
+# index_num   0        +1      +2        +3
+#                      -3       -2       -1
+# print(students[2].title())
+
+# print(students[0].upper())
+
+
+# print(students[3].lower())
+
+
+students[-1]
+
+print(f"The name of student is: {result}")
+
+
