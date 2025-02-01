20a935c4ba8aec2b8d3b0c5f5371e4dd4b17b0ee
tajinstituteofit <taj.instit@gmail.com>
2025-02-01 12:55:14 +0500

    Add files via upload

---
+++ Class wise Source Code/Class 9/main.py
@@ -0,0 +1,44 @@
+# Float
+
+# abc =  10.50  +  3.32
+# print(abc)
+
+
+# cde = 0.2 * 0.5
+# print(cde)
+
+# # num
+# 10 + 10
+
+
+# # str
+# "10" "The name of man is etc"
+
+
+# Integers and Floats
+
+
+# a = 2.8
+# b = 10
+
+
+
+# print(type(a))
+# print(type(b))
+
+
+# Underscores in Numbers
+
+# e = 5_0
+# f = 1
+# print(e + f)
+
+abc_def  = 50_100
+print(abc_def + 1)
+
+
+# Multiple Assignment
+
+# a, b, c = 20, 30, "The name of student is etc"
+
+# print(b, c)
