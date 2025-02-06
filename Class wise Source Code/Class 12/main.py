e01c91d08b26dca4a9d5954989c2340e05e14b65
tajinstituteofit <taj.instit@gmail.com>
2025-02-06 07:53:13 +0500

    Add files via upload

---
+++ Class wise Source Code/Class 12/main.py
@@ -0,0 +1,26 @@
+# Using Individual Values from a List
+
+
+abc = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+students = ['Rehan Ahmed', 'Saifulislam']
+
+# cde = students[0].title()
+# print(f"The list of sudents is: {students}")
+
+# result = abc[1]
+
+
+# final_result = f"There are {result} students in class and the name of students is : {students[0].upper()} and {students[1].lower()}"
+# print(f"Response: \n{final_result}")
+
+
+
+# Modifying, Adding, and Removing Elements
+
+print(students)
+
+students[0] = 'Rehan'
+students[1] = 'Saif'
+
+print(students)
+
