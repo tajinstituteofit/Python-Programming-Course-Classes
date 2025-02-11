a3410cad6316a0a18b982a333b491dce8c606bde
tajinstituteofit <taj.instit@gmail.com>
2025-02-11 13:16:59 +0500

    Add files via upload

---
+++ Class wise Source Code/Class 13/main.py
@@ -0,0 +1,53 @@
+students = ['ali', 'usama', 'yasir', '10']
+print(students)
+
+
+xyz = students[3]
+# print(xyz + 20)
+
+print(type(xyz))     # Type in string
+
+result = int(xyz)    # Type change into integer
+
+print(type(result))
+
+print(result + 20)
+
+students[1] = "abdullah"    # Modify the value of list
+print(students)
+
+
+abc = 'hanzla'
+
+students.append(abc)
+
+
+print(students)
+
+print("==============================================================")
+
+
+results = []
+
+one = "usaman"
+two = "ali"
+
+efg = (one, two)
+
+results.append(efg)
+print(results)
+
+# print(int(result[0]))
+
+print("=============================================================")
+
+ali = result[0][1]
+print(ali)
+
+# cde = results[0]
+
+# bottle = cde[0]
+# print(bottle)
+# print(cde)
+
+
