e36ef9985299757340ea950274e8554d6a27132b
tajinstituteofit <taj.instit@gmail.com>
2025-02-12 18:59:11 +0500

    Add files via upload

---
+++ Class wise Source Code/Class 14/main.py
@@ -0,0 +1,22 @@
+# Inserting Elements into a List
+
+students = ['ali', 'usama', 'yasir', '10']
+
+print(students)
+
+students.insert(0, 'shahroz')
+
+print(students)
+
+del students[4]
+
+print(students)
+
+abc = [1,2,3,4,5,6,7]
+
+students.remove('shahroz')
+
+print(students)
+
+
+
---
+++ Class wise Source Code/Class 15/main.py
@@ -0,0 +1,34 @@
+# Removing an Item Using the pop() Method
+
+
+# students = ['ali', 'usama', 'yasir', '10']
+# print(students)
+
+# result = students.pop(1)
+
+# print(f"The value is deleted: {result}")
+
+# print(students)
+# print(result)
+
+
+# students.remove('ali')
+
+# print(students)
+
+# del  values remove through index number
+# pop  also remove values through index number and without index number. One additional functionality is pop can store the value that it is deleted.
+# remove: Remove the value in a list but it take a value not index number
+
+
+# motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+# print(motorcycles)
+# too_expensive = 'ducati'
+# motorcycles.remove(too_expensive)
+# print(motorcycles)
+# print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+import this
+
+
+
