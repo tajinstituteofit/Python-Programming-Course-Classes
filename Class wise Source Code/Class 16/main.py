591ace98839e5dbd2e0ab6d9cdd0fe5974926acf
tajinstituteofit <taj.instit@gmail.com>
2025-02-13 18:18:02 +0500

    Add files via upload

---
+++ Class wise Source Code/Class 16/main.py
@@ -0,0 +1,39 @@
+# #  Organizing a List
+
+# name = input("what is your name ")
+
+# # print(name)
+
+students = ['ali', 'yasir', 'usama', 'arslan', 'ubaid']
+
+# print(students)
+
+# students.sort() #  Sort function shuffle the value in ascending order means start from (a) and then next till that (z) and sort perment
+
+# print(students)
+
+# print("===========================================================================")
+
+# students.sort(reverse=True)  # reverse function reverse the whole list
+
+# print(students)
+
+# print("===========================================================================")
+
+print(sorted(students))   # The sorted function just sort the data in ascending order as per our needs and not perment change the list
+
+print(students)
+
+
+employ = ['ali', 'yasir', 'usama', 'arslan', 'ubaid']
+
+# print(employ)
+
+employ.reverse()
+
+# print(employ)
+
+
+# print(len(employ))
+xyz = len(employ)   # len function find the lenght of the list and start with 1 and then move forward like 2,3,4.... 
+print(xyz)
