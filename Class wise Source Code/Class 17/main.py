6b0f71ec3b09df9ab28b9cb11581761fb69fb081
tajinstituteofit <taj.instit@gmail.com>
2025-02-16 14:43:21 +0500

    Add files via upload

---
+++ Class wise Source Code/Class 17/main.py
@@ -0,0 +1,21 @@
+# Avoiding Index Errors When Working with Lists
+
+
+# names = ['John', 'Alice', 'Bob']
+
+# print(names[3])
+
+# name = 'ali'
+
+# result = 2 + name
+# print(result)
+
+
+# names = ['John', 'Alice', 'Bob']
+
+# print(names[-1])
+
+
+abc = []
+
+print(abc[1])
