cad566fa77130b3d7e84fad6d20e807e470709c5
tajinstituteofit <taj.instit@gmail.com>
2025-03-19 13:00:49 +0500

    Add files via upload

---
+++ Class wise Source Code/Class 18/main.py
@@ -0,0 +1,20 @@
+# Chapter 4 (Working with lists)
+
+abc = [1,2,3,4,5,6,7,8,9]
+
+
+
+# print(f"The value is: {abc}")
+
+
+# For Loop
+# for result in abc:
+#     print(f"The value is: {result}")
+
+
+students = ['hamza', 'usama', 'ameen', 'shahzaib','hamza', 'usama', 'ameen', 'shahzaib','hamza', 'usama', 'ameen', 'shahzaib','hamza', 'usama', 'ameen', 'shahzaib']
+
+
+for keys in students:
+    print(f"ALl students have there like: {keys.title()}")
+    
