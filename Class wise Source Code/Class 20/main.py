20782412bcbd38f3d0e7718c08de428f33fe7927
tajinstituteofit <taj.instit@gmail.com>
2025-04-06 14:17:12 +0500

    Add files via upload

---
+++ Class wise Source Code/Class 20/main.py
@@ -0,0 +1,21 @@
+names = ['ali', 'usama', 'yasir', 'arslan']
+
+# for abc in names:
+#     print(abc)
+#     print('Hello')   
+    
+    
+    
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#     print(f"{magician.title()}, that was a great trick!")
+#     print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+# print("Thank you everyone, that was a great magic show!")
+
+
+
+# for cde in names:
+    
+
+#     print(cde)
